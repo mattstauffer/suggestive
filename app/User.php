42a57966ebd1d64cc375d88538e5d61c292f2589
Matt Stauffer <matt@tighten.co>
2015-12-08 17:09:56 -0600

    Cleanup

--- app/User.php
+++ app/User.php
@@ -18,25 +18,10 @@ class User extends Model implements AuthenticatableContract,
 {
     use Authenticatable, Authorizable, CanResetPassword;
 
-    /**
-     * The database table used by the model.
-     *
-     * @var string
-     */
     protected $table = 'users';
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array
-     */
     protected $fillable = ['name', 'email', 'github_id', 'avatar'];
 
-    /**
-     * The attributes excluded from the model's JSON form.
-     *
-     * @var array
-     */
     protected $hidden = ['remember_token'];
 
     public function topics()
